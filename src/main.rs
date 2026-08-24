4cc998b324bc1cef73b617b9100f22787cbdbe2e
eulegang <eulegang@eulegang.dev>
2026-08-24 15:27:20 -0500

    fix: adding model dump env var

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,14 @@ use crate::{model::Model, tui::App};
 fn main() -> Result<()> {
     let mode = Cli::parse_args().diff_mode()?;
     let model = Model::load(&mode)?;
-    let mut app = App::new(&model);
 
-    app.run()
+    if option_env!("DEBUG_MODEL") != None {
+        dbg!(&model);
+
+        Ok(())
+    } else {
+        let mut app = App::new(&model);
+
+        app.run()
+    }
 }
